8b02f8ad0c5ae93cbb5b9da210fb27876f402273
djosix <djosicks@gmail.com>
2020-05-18 16:05:59 +0800

    Update: 主要好像就是更改 port 而已

--- src/main.js
+++ src/main.js
@@ -9,14 +9,14 @@ import * as PIXI from 'pixi.js'
 import * as d3 from 'd3'
 import * as moment from 'moment'
 
-const api_port = process.env.VUE_APP_APIPORT || 5000
+const api_port = process.env.VUE_APP_APIPORT || 80
 console.log(api_port,process.env)
 Vue.prototype.$moment = moment
 Vue.prototype.$d3 = d3
 Vue.prototype.$PIXI = PIXI
 Vue.prototype.$axios = axios
 // Vue.prototype.$api = `http://140.113.210.9:${api_port}/api/v1`
-Vue.prototype.$api = `http://140.113.24.136:${api_port}/api/v1`
+Vue.prototype.$api = `http://140.113.210.26:${api_port}/api/v1`
 // Vue.prototype.$router = router
 //Vue.prototype.$api = 'http://140.113.213.115:50000/api'
 // Vue.prototype.$api = 'http://localhost:5000/api'
